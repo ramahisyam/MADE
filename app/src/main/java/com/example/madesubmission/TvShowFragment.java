4bc37def3c4f6e5e23861c0054e84bb9a23dbc0a
ramahisyam <ramahisyam1@gmail.com>
2019-07-01 16:14:08 +0700

    complete add data list

--- app/src/main/java/com/example/madesubmission/TvShowFragment.java
+++ app/src/main/java/com/example/madesubmission/TvShowFragment.java
@@ -1,23 +1,30 @@
 package com.example.madesubmission;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.madesubmission.adapter.TvShowAdapter;
+import com.example.madesubmission.model.TvShow;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TvShowFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TvShowFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class TvShowFragment extends Fragment {
+    private String[] dataTitle;
+    private String[] dataDate;
+    private TypedArray dataPhoto;
+    private RecyclerView rvTvShow;
+    private ArrayList<TvShow> tvShows = new ArrayList<>();
+
     public TvShowFragment() {
         // Required empty public constructor
     }
@@ -27,5 +34,37 @@ public class TvShowFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tv_show, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        rvTvShow = view.findViewById(R.id.rv_tv_show);
+        rvTvShow.setHasFixedSize(true);
+
+        prepare();
+        addItem();
+    }
+
+    private void prepare() {
+        dataTitle = getResources().getStringArray(R.array.tv_show_title);
+        dataDate = getResources().getStringArray(R.array.tv_show_release);
+        dataPhoto = getResources().obtainTypedArray(R.array.tv_show_photo);
+    }
+
+    private void addItem() {
+        for (int i = 0; i < dataTitle.length; i++) {
+            TvShow tvShow = new TvShow();
+            tvShow.setPhoto(dataPhoto.getResourceId(i, -1));
+            tvShow.setTitle(dataTitle[i]);
+            tvShow.setReleaseDate(dataDate[i]);
+            tvShows.add(tvShow);
+        }
+
+        rvTvShow.setLayoutManager(new LinearLayoutManager(getActivity()));
+        TvShowAdapter adapter = new TvShowAdapter(getActivity());
+        adapter.setListTvShow(tvShows);
+        rvTvShow.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/madesubmission/adapter/TvShowAdapter.java
+++ app/src/main/java/com/example/madesubmission/adapter/TvShowAdapter.java
@@ -1,4 +1,68 @@
 package com.example.madesubmission.adapter;
 
-public class TvShowAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.madesubmission.R;
+import com.example.madesubmission.model.Movie;
+import com.example.madesubmission.model.TvShow;
+
+import java.util.ArrayList;
+
+public class TvShowAdapter extends RecyclerView.Adapter<TvShowAdapter.TvShowViewHolder>{
+    private Context context;
+    private ArrayList<TvShow> listTvShow;
+
+    public TvShowAdapter(Context context) {
+        this.context = context;
+    }
+
+    public ArrayList<TvShow> getListTvShow() {
+        return listTvShow;
+    }
+
+    public void setListTvShow(ArrayList<TvShow> listTvShow) {
+        this.listTvShow = listTvShow;
+    }
+
+    @NonNull
+    @Override
+    public TvShowAdapter.TvShowViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_row, viewGroup, false);
+
+        return new TvShowViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TvShowAdapter.TvShowViewHolder tvShowViewHolder, int i) {
+        tvShowViewHolder.tvTitle.setText(getListTvShow().get(i).getTitle());
+        tvShowViewHolder.tvDate.setText(getListTvShow().get(i).getReleaseDate());
+
+        Glide.with(context).load(getListTvShow().get(i).getPhoto()).into(tvShowViewHolder.imgPhoto);
+    }
+
+    @Override
+    public int getItemCount() {
+        return getListTvShow().size();
+    }
+
+    class TvShowViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle;
+        TextView tvDate;
+        ImageView imgPhoto;
+        TvShowViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvTitle = itemView.findViewById(R.id.tv_title);
+            tvDate = itemView.findViewById(R.id.tv_date);
+            imgPhoto = itemView.findViewById(R.id.img_photo);
+        }
+    }
 }
